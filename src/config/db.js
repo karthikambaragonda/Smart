2fb89b62f8e1f5e24e772568c3c48110080e2031
Karthik Ambaragonda <ambaragondakarthik@gmail.com>
2026-04-03 21:01:38 +0530

    removed secrets

--- src/config/db.js
+++ src/config/db.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 async function connectDB() {
   try {
     await mongoose.connect(
-      process.env.MONGODB_URI || "mongodb://newuser:pJ7M6FAiYuZnNBb8@cluster0-shard-00-00.wkq5i.mongodb.net:27017,cluster0-shard-00-01.wkq5i.mongodb.net:27017,cluster0-shard-00-02.wkq5i.mongodb.net:27017/?ssl=true&replicaSet=atlas-f7mld6-shard-0&authSource=admin&appName=Cluster0"
+      process.env.MONGODB_URI
     );
     console.log("MongoDB connected");
   } catch (error) {
